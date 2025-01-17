aa78bafc8b6eaba3183a25ceaab22da50e1afc23
Simon776-cmd <amatosimon799@gmail.com>
2025-01-17 21:10:45 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,7 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-  ['xxxxxxxxxx', '𝐃𝚲𝐍𝕀𝚵𝐋͎💋', true],
-  ['xxxxxxxxxx',],
-  ['xxxxxxxxxx'],	
-  ['xxxxxxxxxx',],
-  ['xxxxxxxxxx'],
-  ['xxxxxxxxxx'],
+  ['83193763309, true],
 ] //Numero degli owner
 
 //────────────────────────────
